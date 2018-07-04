049cdf7ba6faccd005cb1e83f1f2c664ed79454d
spirinars <spirinars@ya.ru>
2018-07-04 07:41:09 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
-package main.java.ru.job4j.calculate;
+package ru.job4j.calculate;
 
+/**
+ *Class Calculate Решение задачи части [141]
+ *@autor spirinars
+ *since 03.07.2018
+*/ 
 public class Calculate {
-	
+	/**
+	 * Main.
+	 * @oaram args - args.
+	 * Метод main позволяет вывести надпись "Hello World в консоль"
+	*/
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Petr Arsentev (mailto:spirinars@ya.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
