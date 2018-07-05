1ab38c9fd16b04e0aa12dee6c733329c36c30f67
spirinars <spirinars@ya.ru>
2018-07-05 07:29:47 +0300

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Test.
+*
+*@author Spirin Arseny (mailto: spirinars@ya.ru
+*version $id$
+*since 0.1
+*/
+public class CalculateTest{
+/**
+*Test ecgo.
+*/@Test
+public void whenTakeNamaThenTreeEchoPlusName(){
+	String input = "Spirin Arseny";
+	String expect = "Echo, echo, echo : Spirin Arseny";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result, is(expect));
+	}
+	
+}
