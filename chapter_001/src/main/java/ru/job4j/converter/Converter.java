262cd3422e5d37c1444ba363603a93ff13d32459
spirinars <spirinars@ya.ru>
2018-07-09 21:30:37 +0300

    3.2. Конвертер валюты. [#41583]

---
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,45 @@
+package ru.job4j.converter;
+
+/**
+ *Class Converter Решение задачи части [41583]
+ *@author Spirinars mailto:spirinars@ya.ru
+ *since 09.07.2018
+ */
+public class Converter {
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллары
+     */
+    public int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    /**
+     * Конвертируем евро в рубли.
+     * @param value евро.
+     * @return Рубли.
+     */
+    public int euroToRuble(int value) {
+        return value * 70;
+    }
+
+    /**
+     * Конвертируем доллары в рубли.
+     * @param value доллары.
+     * @return Рубли.
+     */
+    public int dollarToRuble(int value) {
+        return value * 60;
+    }
+}
+
