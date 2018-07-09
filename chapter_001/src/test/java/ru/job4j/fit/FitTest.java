f65efd8d44e7fb769a201c377566c176d42d3f3f
spirinars <spirinars@ya.ru>
2018-07-09 21:48:31 +0300

    3.3. Идеальный вес. [#41584]

---
+++ chapter_001/src/test/java/ru/job4j/fit/FitTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.fit;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ *Test.
+ *
+ *@author Spirin Arseny (mailto: spirinars@ya.ru)
+ *version $id$
+ *since 0.1
+ */
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, closeTo(92.0, 0.1));
+    }
+
+    @Test
+    public void womanWeight() {
+        Fit fit = new Fit();
+        double weight = fit.womanWeight(170);
+        assertThat(weight, closeTo(69.0, 0.1));
+    }
+}
