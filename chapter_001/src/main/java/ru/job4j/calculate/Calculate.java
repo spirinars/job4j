6a258fd5e180e3d6d5aaa0024fca9c7586231d6f
spirinars <spirinars@ya.ru>
2018-07-09 07:27:15 +0300

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -4,13 +4,13 @@ package ru.job4j.calculate;
  *Class Calculate Решение задачи части [141]
  *@author Spirinars
  *since 03.07.2018
-*/ 
+ */
 public class Calculate {
 	/**
 	 * Main.
 	 * @param args - args.
 	 * Метод main позволяет вывести надпись "Hello World в консоль"
-	*/
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
