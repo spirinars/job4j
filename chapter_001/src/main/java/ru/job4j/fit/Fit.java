a495008ce3faf0f0f8f195be70163edbf5f1720f
spirinars <spirinars@ya.ru>
2018-07-09 21:55:20 +0300

    3.3. Идеальный вес. [#41584]
    исправил немного что бы checkstyle не ругался

--- chapter_001/src/main/java/ru/job4j/fit/Fit.java
+++ chapter_001/src/main/java/ru/job4j/fit/Fit.java
@@ -13,7 +13,7 @@ public class Fit {
      * @return идеальный вес.
      */
     public double manWeight(double height) {
-        return (height -100)*1.15;
+        return (height - 100) * 1.15;
     }
 
     /**
@@ -22,6 +22,6 @@ public class Fit {
      * @return идеальный вес.
      */
     public double womanWeight(double height) {
-        return (height -110)*1.15;
+        return (height - 110) * 1.15;
     }
 }
