1755ea34a07016b0198386517d30b130fd439a6b
spirinars <spirinars@ya.ru>
2018-07-11 19:43:41 +0300

    4.1. Глупый бот. [#31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+/**
+ *Class Point 4.1. Глупый бот. [#31323]
+ *@author Spirinars mailto:spirinars@ya.ru
+ *@version $Id$
+ *since 11.07.2018
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
+
