7a1d9d7e29388582070a0aa8474e1b9fd1688e6c
spirinars <spirinars@ya.ru>
2018-07-13 08:21:15 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+/**
+ *5.1. Подсчет суммы чётных чисел в диапазоне [#192]
+ *@author Spirinars mailto:spirinars@ya.ru
+ *@version $Id$
+ *since 12.07.2018
+ */
+public class Counter {
+    /**
+     * Method add.
+     * @param start int.
+     * @param finish int.
+     * @return result.              .
+     */
+    public int add(int start, int finish) {
+            int result = 0;
+            for (int index = 0; index <= 10; index++) {
+                if (index % 2 == 0) {
+                    result += index;
+                }
+            }
+            return result;
+        }
+}
+
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *@author Spirinars mailto:spirinars@ya.ru
+ *@version $Id$
+ *since 12.07.2018
+ */
+    public class CounterTest {
+        @Test
+        public void whenSumEvenNumbersFromOneToTenThenThirty() {
+            Counter count = new Counter();
+            int resul = count.add(0, 10);
+            assertThat(resul, is(30));
+        }
+    }
+
